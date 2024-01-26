82661b793ad895eedb311138e4cb638794bb4bf3
3liGonzalez <153216141+3liGonzalez@users.noreply.github.com>
2024-01-25 18:13:56 -0600

    Add files via upload

---
+++ About.jsx
@@ -0,0 +1,14 @@
+import "./About.css";
+const About = () => {
+    return ( 
+        <div>
+      <h1> Nombre: Elizabeth González </h1>
+      <informacionPersonal />
+      <p> Correo/ Contacto: eli.hispanicas.unam@gmail.com</p>
+      <p> Descripcion: Estoy iniciando como desarrolladora Java, interesada en el area de TI</p>
+      { Image }
+    </div>
+
+    );
+};
+export default informacionPersonal
